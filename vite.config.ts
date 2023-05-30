abc785ef254e1ad018a12b5423b38f67916a37cc
keiko233 <keiko23333@gmail.com>
2023-05-30 15:33:46 +0800

    feat: import VueComponents

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,8 @@ export default defineConfig({
         'vue'
       ]
     }),
+    Components({
+      resolvers: []
+    })
   ],
 })
