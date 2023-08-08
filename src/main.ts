191cb5f38c11e96dfc0a867f68eeeb99319db64c
keiko233 <keiko23333@gmail.com>
2023-08-08 20:29:36 +0800

    feat: import  vue-m-message

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import './assets/index.less'
 import App from './App.vue'
 import i18n from './i18n'
 import router from './router'
+import Message, { MessageTypeOptions } from 'vue-m-message'
+import 'vue-m-message/dist/style.css'
 
 // Vuetify
 import 'vuetify/styles'
@@ -11,6 +13,13 @@ import { createVuetify } from 'vuetify'
 const vuetify = createVuetify()
 
 createApp(App)
+  .use(Message, {
+    name: 'msg',
+    defaultOptions: <MessageTypeOptions>{
+      supportHTML: true,
+      zIndex: 3000
+    }
+  })
   .use(vuetify)
   .use(i18n)
   .use(router)
