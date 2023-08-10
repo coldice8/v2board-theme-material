990f81141d74eb3c48e0660d8ab1b4b4d5627e49
keiko233 <keiko23333@gmail.com>
2023-08-11 00:20:02 +0800

    fix: SubscribeInfoCard component when plan null err

--- src/components/SubscribeInfoCard.vue
+++ src/components/SubscribeInfoCard.vue
@@ -1,8 +1,9 @@
 <template>
-  <v-card v-if="userSubscribe" height="100%" class="mx-auto d-flex flex-column align-space-between" :title="`我的订阅 ${userSubscribe.plan.name}`">
+  <v-card v-if="userSubscribe" height="100%" class="mx-auto d-flex flex-column align-space-between" title="我的订阅">
     <v-card-text>
-      <div class="d-flex justify-space-between align-end">
+      <div v-if="userSubscribe.plan" class="d-flex justify-space-between align-end">
         <div>
+          <h3>{{ userSubscribe.plan.name }}</h3>
           <p>到期时间: {{ convertTimestampToDateTime(userSubscribe.expired_at, false) }}</p>
           <p>重置剩余天数: {{ userSubscribe.plan.capacity_limit }}</p>
           <p>已用: {{ formatBytes(userSubscribe.u + userSubscribe.d) }}</p>
@@ -11,6 +12,11 @@
         <v-progress-circular :size="96" :width="16" :model-value="getTransferPercentage(userSubscribe)">
           <b>{{ getTransferPercentage(userSubscribe) }}%</b>
         </v-progress-circular>
+      </div>
+
+      <div v-else>
+        <p>没有订阅信息，请购买订阅</p>
+        <v-btn block rounded prepend-icon="mdi-store-plus" class="mt-2">购买订阅</v-btn>
       </div>
     </v-card-text>
   </v-card>
