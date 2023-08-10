3ec11460648581f3f3fa5799374445cc3ee5edf9
keiko233 <keiko23333@gmail.com>
2023-08-11 00:53:30 +0800

    fix: NavBar list event err

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,20 @@
 <template>
   <v-card class="mt-2 rounded-pill" flat title="Material Board" />
 
-  <v-list mandatory :value="selected">
-    <v-list-item v-for="(item, index) in items" :value="index" :to="item.to" :prepend-icon="item.icon" :title="item.name"
-      rounded="xl" color="blue-darken-4" />
+  <v-list mandatory>
+    <v-list-item v-for="(item, i) in items" :key="i" :value="item" :prepend-icon="item.icon" :title="item.name"
+      color="blue-darken-4" rounded="xl" @click="click(item.to)" />
   </v-list>
 </template>
 
 <script setup lang="ts">
-const selected = ref(0);
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const items = ref([
   { name: '主页', to: '/dashboard', icon: 'mdi-monitor-dashboard' },
 ]);
+
+const click = (to: string) => router.push(to);
 </script>
