edf83803cac2a372ec692d9dfa014b4df112f4a9
keiko233 <keiko23333@gmail.com>
2023-10-15 15:06:13 +0800

    faet: LoadingMask support prop

--- src/components/LoadingMask.vue
+++ src/components/LoadingMask.vue
@@ -1,6 +1,15 @@
 <template>
-  <v-overlay :model-value="true" persistent class="align-center justify-center text-white">
+  <v-overlay :model-value="props.show" persistent class="align-center justify-center text-white">
     <v-progress-circular indeterminate size="64" />
     <p class="mt-2">Loading...</p>
   </v-overlay>
 </template>
+
+<script setup lang="ts">
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: true
+  }
+})
+</script>
