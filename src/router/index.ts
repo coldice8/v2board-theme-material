8e36045428af5affb4c460e194812a61ae0cd81a
keiko233 <keiko23333@gmail.com>
2023-10-15 14:26:28 +0800

    fix: router name typos & router name missing

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,35 +13,41 @@ const routes = [
   },
   {
     path: '/dashboard',
-    name: 'Dashboard View',
     component: () => import("../views/DashboardView.vue"),
     children: [
       { 
         path: '', 
+        name: 'Dashboard',
         component: () => import("../views/Dashboard/HomeView.vue") 
       },
       { 
         path: 'plan', 
+        name: 'Plan',
         component: () => import("../views/Dashboard/PlanView.vue") 
       },
       { 
         path: 'node', 
+        name: 'Node',
         component: () => import("../views/Dashboard/NodeView.vue") 
       },
       { 
         path: 'order', 
+        name: 'Order',
         component: () => import("../views/Dashboard/OrderView.vue") 
       },
       { 
         path: 'profile', 
+        name: 'Profile',
         component: () => import("../views/Dashboard/ProfileView.vue") 
       },
       { 
         path: 'traffic', 
+        name: 'Traffic',
         component: () => import("../views/Dashboard/TrafficView.vue") 
       },
       { 
         path: 'ticket', 
+        name: 'Ticket',
         component: () => import("../views/Dashboard/TicketView.vue") 
       },
     ]
